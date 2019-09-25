a7abf28a9e665d6c5fe658f31eec7f1118c12ece
Dream High <dreamhigh0822@outlook.com>
2019-09-25 06:17:28 -0400

    modified pagniation

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Pagination } from 'react-bootstrap';
 
 export default function({currentPage, totalCount, onChange}) {
+    const WINDOW_SIZE = 6;
+    const [pageWindow, setPageWindow] = useState({start: 1, end:WINDOW_SIZE });
     const pageCount = Math.ceil(totalCount / 10) || 1;
+
+    useEffect(() => {
+        if (pageCount > WINDOW_SIZE) {
+            setPageWindow({start: currentPage, end: currentPage + WINDOW_SIZE});
+        }
+    }, []);
+
     const onPageChange = (pageNum) => () => {
-        onChange(pageNum)
+        onChange(pageNum);
     }
 
     const onPrev = () => {
+        if ((currentPage - 1) < pageWindow.start) {
+            setPageWindow(prev => { return {start: prev.start - 1, end: prev.end - 1} });
+        } 
         onPageChange(currentPage - 1)();
     }
 
     const onNext = () => {
+        if ((currentPage + 1) > pageWindow.end) {
+            setPageWindow(prev => { return {start: prev.start + 1, end: prev.end + 1} });
+        } 
         onPageChange(currentPage + 1)();
     }
 
     const onFrist = () => {
+        setPageWindow(prev => { return {start: 1, end: WINDOW_SIZE}})
         onPageChange(1)();
     }
 
     const onLast = () => {
+        setPageWindow(prev => { return {start: pageCount - WINDOW_SIZE + 1, end: pageCount} });
         onPageChange(pageCount)();
     }
 
@@ -28,8 +45,13 @@ export default function({currentPage, totalCount, onChange}) {
             <Pagination.First disabled={currentPage === 1} onClick={onFrist} />
             <Pagination.Prev disabled={currentPage === 1} onClick={onPrev} />
             {
-                Array(pageCount).fill().map((item, index) => {
-                    const pageNum = index + 1;
+                pageWindow.start === 1 || pageCount < WINDOW_SIZE ? (
+                    null
+                ) : <Pagination.Ellipsis />
+            }
+            {
+                Array(pageCount < WINDOW_SIZE ? pageCount : WINDOW_SIZE).fill().map((item, index) => {
+                    const pageNum = pageWindow.start + index;
                     return (
                         <Pagination.Item
                             key={`page_${pageNum}`}
@@ -40,7 +62,12 @@ export default function({currentPage, totalCount, onChange}) {
                         </Pagination.Item>
                     )
                 })
-            }  
+            } 
+            {
+                pageWindow.end === pageCount || pageCount < WINDOW_SIZE ? (
+                    null
+                ) : <Pagination.Ellipsis />
+            } 
             <Pagination.Next disabled={currentPage === pageCount} onClick={onNext} />
             <Pagination.Last disabled={currentPage === pageCount} onClick={onLast}/>
         </Pagination>  
